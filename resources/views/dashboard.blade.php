007de1dd0ca4455862a4368c7b03287cf1a52d39
NOL\huan3 <jdelgadod001@alumno.uaemex.mx>
2024-08-15 16:32:14 -0600

    iNICIO DE VISTAS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,10 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <h1 class="text-2xl font-bold text-gray-900 dark:text-gray-100 text-center">
+                    APLICACIÓN DE PUBLICACIONES CRUD
+                </h1>
             </div>
         </div>
     </div>
